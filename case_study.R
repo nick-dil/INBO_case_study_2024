15052891bd28c4ea5465acc1bf6dc2d3e6fc8c9a
nick-dil <nick.dillen@tuta.io>
2024-02-04 23:53:50 +0100

    All objectives answered, cleanup and doc

--- case_study.R
+++ case_study.R
@@ -1,77 +1,153 @@
 #######################
 ### CASE STUDY INBO ###
-#######################
-
+####################### 
 # By Nick Dillen
 
+# Required packages
+# Use `install.packages("<>")` if not installed
+library(rgbif)
+library(geojsonio)
+library(ggplot2)
+library(sf)
+library(ggspatial)
+library(leaflet)
+
+
+#####
+# ASSIGNMENT:
 # A recurring question you get from invasive species researchers is to produce a map or bar chart
 # per year of known observations of Asian hornets (Vespa velutina) in Flanders. They typically
 # want to filter this information on a year range.
 
-## Data retrieval
+#####
+# DATA ACQUISITION
 # 1. To define a year range (e.g. 2019-2021).
 # 2. To retrieve occurrences from the Global Biodiversity Information Facility (GBIF) for that
 # year range of Vespa velutina in Flanders.
 
 
-# Use R API for GBIF dataset
-#install.packages("rgbif") # CRAN version
-library(rgbif)
-
-# Look up taxon key for "Vespa velutina"
+# Use R API for GBIF dataset: rgbif
+# First look up taxon key for "Vespa velutina"
 taxonKey <- name_backbone("Vespa velutina")$usageKey
-# Look up country code for Belgium, I did this manually
-# enumeration_country()
+# Look up country code for Belgium, I did this manually from:
+# enumeration_country() #-> "BE"
 
-caseStudyDf = occ_data(taxonKey = taxonKey,
+# Get data from GBIF. NOTE!: For serious research projects use rgbif::occ_download()
+caseStudyData = occ_data(taxonKey = taxonKey,
                        country = "BE",
                        year = "2019,2021",
                        hasCoordinate =TRUE,
                        limit = 100000)
 
-# Check data from FLANDERS only
-# TODO!
-
-# Quick and dirty: check $stateProvince
-table(caseStudyDf$data$stateProvince, useNA = "ifany") 
-# for this dataset we can manually make a list of Flanders provinces
+# SELECT DATA FROM FLANDERS ONLY
+# Quick and dirty approach: use the $stateProvince column to make a selection of the data for "Vlaanderen" only
+table(caseStudyData$data$stateProvince, useNA = "ifany") 
+# for this dataset we can manually make a list of Flanders provinces based on table above
 flanders = c("Antwerp", "East Flanders", "Flemish Brabant", "Limburg", "Vlaanderen", "West Flanders")
-dataFlanders = caseStudyDf$data[caseStudyDf$data$stateProvince %in% flanders,]
+# Then make a slice to only include entries from Flanders
+dataFlanders = caseStudyData$data[caseStudyData$data$stateProvince %in% flanders,]
+print(nrow(dataFlanders))
+# [1] 2124
 
-# Note, a lot of data has NA values for $stateProvince, this gets lost (887 records)
+# Note, a lot of observations have NA for $stateProvince (887 records), this data would be excluded even if
+# it would be in Flanders.
+# A more robust way to detect observations in Flanders might be to check coordinates against a Flanders polygon shape.
+# You can donload that shape data from https://gadm.org/ -> Belgium (level 1 or greater)
+# also used some articles; https://r-graph-gallery.com/168-load-a-shape-file-into-r.html
+# https://r-spatial.org/r/2018/10/25/ggplot2-sf.html
 
-# 4. To present the result as a bar chart of observations per year.
-# basic bar plot
+# set working directory accordingly to read in shape data downloaded from GADM
+spBE <- geojson_read("geojson/gadm41_BEL_2.json",  what = "sp")
+# Quick plot to check we have what we need
+# plot(spBE)
+
+# use SF as it facilitates using ggplot later on
+sfBE = st_as_sf(spBE)
+#Only retain Flanders
+sfFLanders = sfBE[sfBE$NAME_1 == "Vlaanderen",]
+# ggplot() + geom_sf(data=sfFLanders)
+
+# Now we can filter the observation data to exclude entries with coordinates that are outside of the Flanders polygon
+# Convenient way to do this is by also converting the data to an sf object
+dataBE = st_as_sf(caseStudyData$data, coords = c("decimalLongitude","decimalLatitude"))
+# set CRS to WGS 84
+st_crs(dataBE) = st_crs(sfFLanders)
+# and finally filter data based on 'sfFlanders' polygon. NOTE: this overwrites the data in 'dataFlanders'
+dataFlanders = st_filter(dataBE, sfFLanders)
+print(nrow(dataFlanders))
+# [1] 2148
+
+# We gained 2148-2124=24 data points 
+# Also note that this code can be rerun and will (generally) be able to identify data points from Flanders
+# even if other languages or spelling are used in the $stateProvince column
+
+#####
+# DATA VISUALISATION
+# A. To present the result as a bar chart of observations per year.
+# basic bar plot from tabulation of $year
 year_table = table(dataFlanders$year)
 barplot(year_table,
         main="Observations of Vespa velutina in Belgium from 2019 to 2021",
         xlab = "Year", ylab = "Observation count")
 
-#map
-library(geojsonio)
-library(mapproj)
-library(ggspatial)
-library(sp)
-library(broom)
-library(ggplot2)
-library(sf)
-# download from https://gadm.org/
-# ref; https://r-graph-gallery.com/168-load-a-shape-file-into-r.html
-# https://r-spatial.org/r/2018/10/25/ggplot2-sf.html
-spdf <- geojson_read("geojson/gadm41_BEL_2.json",  what = "sp")
-
-# use SF as nicely integrated with ggplot
-sfBE = st_as_sf(spdf)
-sfFLanders = sfBE[sfBE$NAME_1 == "Vlaanderen",]
-#Only retain Flanders
-ggplot() + geom_sf(data = sfFLanders)
-
-ggplot() +
+# B. To present the result as a map of observations
+# We build a ggplot object with the shape file of Flanders as a polygon and plot the observations
+# from our selected data as points
+vvmap = ggplot() +
   geom_sf(data = sfFLanders) +
+  geom_sf(data = dataFlanders, shape=1) +
   ggtitle("Vespa velutina observations in Flanders (2019-2021)") +
   xlab("Longitude") + ylab("Latitude") +
-  geom_point(data = dataFlanders, aes(x = decimalLongitude, y=decimalLatitude)) +
-  coord_sf(ylim = c(50.5, 51.6))+
+  coord_sf(ylim = c(50.5, 51.6)) +
   annotation_scale(location = "bl",width_hint = 0.2, pad_x = unit(2,"cm")) +
   theme_bw()
 
+vvmap
+
+# We can also make an interactive map with leaflet, a lot of options here. I chose to include the $occurrenceRemarks 
+# and $year data to give a little bit more info about the data points. If you click on an observation in the leaflet
+# map a popup should appear with the appropriate text. A white stroke corresponds to non-NA (interesting) remark
+
+leaflet(data=dataFlanders) %>% addTiles() %>%
+  addCircleMarkers(stroke = ~!is.na(occurrenceRemarks),
+                   fillOpacity = 0.5,
+                   opacity = 0.5 ,
+                   fillColor  = 'navy',
+                   color = 'white',
+                   popup = ~paste0(year, " - ",occurrenceRemarks))
+
+
+
+#####
+# Extra
+# We can do a small check of the observations with possible "mislabeled" $stateProvince. This is easier in leaflet
+# because its interactive and you can zoom in
+table(dataFlanders$stateProvince, useNA = "ifany") # some (4) "Waalse" observations and 24 'NA'.
+# Could call them False Negatives if we take the more "robust" method used above as truth (spoiler: its not)
+FNdata = dataFlanders[!dataFlanders$stateProvince %in% flanders,]
+# but also the other way around, points labelled as "vlaanderen" but coordinates outside the shape file exist
+# Call them False Positives
+FPdata = st_as_sf(
+  caseStudyData$data[caseStudyData$data$stateProvince %in% flanders & !caseStudyData$data$key %in% dataFlanders$key,],
+  coords = c("decimalLongitude","decimalLatitude")
+)
+st_crs(FPdata) = st_crs(sfFLanders)
+
+
+
+# visualize predicted FN and FP on map. Also load shapefile of Belgium to verify.
+leaflet(sfBE) %>% addTiles() %>% addPolygons(stroke = TRUE, 
+                                             label = ~paste(NAME_2)
+                                             ) %>%
+  addCircleMarkers(data = FPdata, stroke = FALSE, fillOpacity = 0.5, color='red',
+                   label=~paste0(stateProvince)) %>%
+  addCircleMarkers(data = FNdata, stroke = FALSE, fillOpacity = 0.5, color='black', label=~paste0(stateProvince))
+  
+
+## !ATTENTION! it seems that the shapefile is low resolution, resulting in some false positives/negatives when 
+# using it to select data from Flanders only! Everything with an nonNA $stateProvince is correctly labeled. 
+# But there are also some observations the have an NA $stateProvince that should be in the Flanders selection. 
+# I need to get a higher res shape of the Belgian regions to resolve this correctly.
+
+  
+ 
