6df88a2938b266a36145b8174396b35dad6544bc
nick-dil <nick.dillen@tuta.io>
2024-02-04 13:50:13 +0100

    added quick flanders filter and first working map of observations

--- case_study.R
+++ case_study.R
@@ -32,24 +32,46 @@ caseStudyDf = occ_data(taxonKey = taxonKey,
 # Check data from FLANDERS only
 # TODO!
 
+# Quick and dirty: check $stateProvince
+table(caseStudyDf$data$stateProvince, useNA = "ifany") 
+# for this dataset we can manually make a list of Flanders provinces
+flanders = c("Antwerp", "East Flanders", "Flemish Brabant", "Limburg", "Vlaanderen", "West Flanders")
+dataFlanders = caseStudyDf$data[caseStudyDf$data$stateProvince %in% flanders,]
+
+# Note, a lot of data has NA values for $stateProvince, this gets lost (887 records)
+
 # 4. To present the result as a bar chart of observations per year.
 # basic bar plot
-year_table = table(caseStudyDf$data$year)
+year_table = table(dataFlanders$year)
 barplot(year_table,
         main="Observations of Vespa velutina in Belgium from 2019 to 2021",
         xlab = "Year", ylab = "Observation count")
 
 #map
 library(geojsonio)
+library(mapproj)
+library(ggspatial)
 library(sp)
 library(broom)
-spdf <- geojson_read("geojson/gadm41_BEL_1.json",  what = "sp")
-plot(spdf)
-newdf =tidy(spdf)
+library(ggplot2)
+library(sf)
+# download from https://gadm.org/
+# ref; https://r-graph-gallery.com/168-load-a-shape-file-into-r.html
+# https://r-spatial.org/r/2018/10/25/ggplot2-sf.html
+spdf <- geojson_read("geojson/gadm41_BEL_2.json",  what = "sp")
+
+# use SF as nicely integrated with ggplot
+sfBE = st_as_sf(spdf)
+sfFLanders = sfBE[sfBE$NAME_1 == "Vlaanderen",]
+#Only retain Flanders
+ggplot() + geom_sf(data = sfFLanders)
 
 ggplot() +
-  geom_polygon(data = newdf, aes( x = long, y = lat, group = group), fill="white", color="grey") +
-  geom_point(data = caseStudyDf$data, aes(x = decimalLongitude, y=decimalLatitude))
-
-ggplot(caseStudyDf$data, aes(x = decimalLongitude, y=decimalLatitude)) + geom_point() +geom_ma
+  geom_sf(data = sfFLanders) +
+  ggtitle("Vespa velutina observations in Flanders (2019-2021)") +
+  xlab("Longitude") + ylab("Latitude") +
+  geom_point(data = dataFlanders, aes(x = decimalLongitude, y=decimalLatitude)) +
+  coord_sf(ylim = c(50.5, 51.6))+
+  annotation_scale(location = "bl",width_hint = 0.2, pad_x = unit(2,"cm")) +
+  theme_bw()
 
